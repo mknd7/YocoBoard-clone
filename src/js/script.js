7fc3a309795a07533daebfee1528d129a8ddc78c
Mukund <mkndviji@gmail.com>
2018-03-26 12:29:18 +0530

    Add function for filler height

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-$(function () {});
+const $filler = $('#filler');
+
+function getHeight() {
+    var height = window.innerHeight;
+    var offset = $filler.offset().top;
+    return (offset < height) ? height - offset : 0;
+}
+
+$(function () {
+
+    $filler.height(getHeight());
+
+    $(window).resize(function() {
+        $filler.height(getHeight());
+    });
+});
